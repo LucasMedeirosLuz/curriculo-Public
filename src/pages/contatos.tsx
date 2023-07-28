44595e9edfee02e43750e7547dfd29d84a21f9fc
LucasMLuz <lucasdmluz@hotmail.com>
2023-07-27 23:48:47 -0300

    contatosLInks

--- src/pages/contatos.tsx
+++ src/pages/contatos.tsx
@@ -7,8 +7,30 @@ const Contatos = () => {
       <title>Contatos | LucasMLuz</title>
     </Head>
     <div>
-      
+      <h1>Contatos</h1>
+      <ul>
+        <li>
+          <span>E-mail</span>
+          <div>
+            <a href="mailto:lucasdmluz@hotmail.com">lucasdmluz@hotmail.com</a>
+          </div>
+        </li>
+        <li>
+          <span>GitHub</span>
+          <div>
+            <a href="https://github.com/LucasMedeirosLuz">https://github.com/LucasMedeirosLuz</a>
+          </div>
+        </li>
+        <li>
+          <span>Linkedin</span>
+          <div>
+            <a href="https://www.linkedin.com/in/lucas-de-medeiros-luz-33b85422b/">https://www.linkedin.com/in/lucas-de-medeiros-luz-33b85422b/</a>
+          </div>
+        </li>
+      </ul>
     </div>
     </>
   )
 };
+
+export default Contatos;
