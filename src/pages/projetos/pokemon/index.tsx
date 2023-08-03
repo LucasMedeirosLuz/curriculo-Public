a869de3d6b3e9faff844d27dc7de0e7a6429ba26
LucasMLuz <lucasdmluz@hotmail.com>
2023-08-02 22:13:22 -0300

    apichamandobonitinha

--- src/pages/projetos/pokemon/index.tsx
+++ src/pages/projetos/pokemon/index.tsx
@@ -1,6 +1,38 @@
 import Head from "next/head";
 
-const Pokemon = () => {
+// const apipoke = async () => {
+//   const maxPokemons = 251
+//   const api = 'https://pokeapi.co/api/v2/pokemon/'
+
+//   const res = await fetch(api, { cache: 'no-store' })
+//   const data = await res.json()
+//   console.log(res)
+//   console.log(data);
+  
+// }
+// apipoke();
+
+export async function getStaticProps() {
+
+  const maxPokemons = 251
+  const api = 'https://pokeapi.co/api/v2/pokemon'
+
+  const res = await fetch(`${api}/?limit=${maxPokemons}`)
+  const data = await res.json()
+
+  data.results.forEach((item: { id: any; }, index: number) => {
+    item.id = index + 1
+  })
+
+  return {
+    props: {
+      pokemons: data.results,
+    } 
+  }
+
+}
+
+export default function pokeindex() {
   return(
     <>
     <Head>
@@ -10,5 +42,3 @@ const Pokemon = () => {
     </>
   )
 };
-
-export default Pokemon;
