185cca4a8cb8839a736940309abf9aa573577b27
LucasMLuz <lucasdmluz@hotmail.com>
2023-08-04 19:20:23 -0300

    requisobre

--- src/pages/projetos/pokemon/[sobrepokemon].tsx
+++ src/pages/projetos/pokemon/[sobrepokemon].tsx
@@ -1,3 +1,31 @@
+export const getStaticPaths = async() => {
+
+  const maxPokemon = 251;
+  const api = 'https://pokeapi.co/api/v2/pokemon/'
+  const res = await fetch(`${api}/?limit=${maxPokemon}`);
+  const data = await res.json();
+
+  const paths = data.results.map((pokemon, index) => {
+    return {
+      params: {pokemonId: (index + 1).toString()},
+    }
+  });
+
+  return {
+    paths,
+    fallback: false,
+  }
+};
+
+export const getStaticProps = async(context) => {
+
+  const id = context.patams.pokemonId;
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+
+  
+  
+};
+
 const Sobre = (pokemon) => {
   return (
     <h1>{`Sobre ${pokemon.id}`}</h1>
