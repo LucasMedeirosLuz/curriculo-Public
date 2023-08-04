cdb99c3f593a8d230ad15a47e78e0dd98b689f38
LucasMLuz <lucasdmluz@hotmail.com>
2023-08-04 20:31:23 -0300

    imageSoblreProntopoucocss

--- src/pages/projetos/pokemon/[sobrepokemon].tsx
+++ src/pages/projetos/pokemon/[sobrepokemon].tsx
@@ -1,15 +1,17 @@
+import Image from "next/image";
+
 export const getStaticPaths = async() => {
 
   const maxPokemon = 251;
-  const api = 'https://pokeapi.co/api/v2/pokemon/'
+  const api = 'https://pokeapi.co/api/v2/pokemon/';
   const res = await fetch(`${api}/?limit=${maxPokemon}`);
   const data = await res.json();
 
   const paths = data.results.map((pokemon, index) => {
     return {
-      params: {pokemonId: (index + 1).toString()},
+      params: {sobrepokemon: (index + 1).toString() },
     }
-  });
+  })
 
   return {
     paths,
@@ -19,16 +21,33 @@ export const getStaticPaths = async() => {
 
 export const getStaticProps = async(context) => {
 
-  const id = context.patams.pokemonId;
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+  const id = context.params.sobrepokemon;
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  const data = await res.json();
 
-  
-  
+  return {
+    props: { pokemon: data},
+  }
 };
 
-const Sobre = (pokemon) => {
+// getStaticProps(0);
+
+const Sobre = ({pokemon}) => {
   return (
-    <h1>{`Sobre ${pokemon.id}`}</h1>
+    <>
+    <div className="flex justify-center content-center text-3xl">
+      <h1>{`Sobre ${pokemon.name}`}</h1>
+    </div>
+    <div className="flex justify-center content-center">
+    <Image
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
+        width={400}
+        height={400}
+        alt={`Imagem do Pokemon ${pokemon.name}`}
+        className="w-56 md:w-80"
+      />
+    </div>
+    </>
   )
 };
 
