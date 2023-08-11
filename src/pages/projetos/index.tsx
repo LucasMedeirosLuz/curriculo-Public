c4b01a9ab2dcd5dd336c22a55d27a969132fd6d8
LucasMLuz <lucasdmluz@hotmail.com>
2023-08-11 16:40:40 -0300

    ...

--- src/pages/projetos/index.tsx
+++ src/pages/projetos/index.tsx
@@ -10,7 +10,7 @@ const Projetos = () => {
     <h1 className="flex text-4xl justify-center my-6">Projetos</h1>
     <div className="flex justify-center text-2xl gap-10">
       <Link className="bg-l-blue-5 rounded p-2" href="/projetos/pokemon">Pokemon</Link>
-      <Link className="bg-l-blue-5 rounded p-2" href="/projetos/loginPage">Pagina de Login</Link>
+      {/* <Link className="bg-l-blue-5 rounded p-2" href="/projetos/loginPage">Pagina de Login</Link> */}
     </div>
     </>
   )
